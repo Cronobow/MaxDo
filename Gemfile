9b7f0a2dc08428e55077c079403cadf50e5211d2
Max Hsieh <crono0720@gmail.com>
2017-11-11 00:26:18 +0800

    add CRUD

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 
 gem 'bootstrap-sass', '~> 3.3.6'
+gem 'font-awesome-rails', '~> 4.7', '>= 4.7.0.2'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,62 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to tasks_path
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attributes(task_params)
+
+    redirect_to tasks_path
+  end
+
+  def done
+    @task = Task.find(params[:id])
+    @task.done = true
+    @task.save
+
+    redirect_to tasks_path
+  end
+
+  def not_yet
+    @task = Task.find(params[:id])
+    @task.done = false
+    @task.save
+
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+
+    if Date.today <= @task.duedate
+      @task.destroy
+    end
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :note, :duedate, :done)
+  end
+
 end
